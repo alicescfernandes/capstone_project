e24bd6b03a70662547a5800d26fca80f7dfd2dd9
Alice Fernandes <alicescfernandes@gmail.com>
2025-06-14 15:09:57 +0100

    alot more report

--- docs/final_report/docker.bat
+++ docs/final_report/docker.bat
@@ -1,3 +1,4 @@
 :: Run this with powershell
-docker build -t compiler_image .
-docker run --rm -v "$(pwd):/data" -w /data compiler_image
+docker build -t alicescfernandes/compiler_image:latest .
+docker push alicescfernandes/compiler_image:latest
+docker run --rm -v "$(pwd):/data" -w /data alicescfernandes/compiler_image:latest
